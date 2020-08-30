16b4bc629b7a9bd7e78bdecaf9509e72f4c3c680
Shangyi Chen <shangyichen@shangyis-air.lan>
2020-08-29 20:00:31 -0700

    second

--- src/app/posts/posts-detail/posts-detail.component.ts
+++ src/app/posts/posts-detail/posts-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { PostService } from '../post.service';
 import { AuthService } from '../../core/auth.service';
@@ -14,41 +14,52 @@ export class PostsDetailComponent implements OnInit {
   post: Post
   editing: boolean = false
 
+  @ViewChild("myDiv") divView: ElementRef;
+
   constructor(
   	private route: ActivatedRoute,
     private router:Router,
     public auth:AuthService,
   	private postService: PostService,
+    private hostElement: ElementRef,
   ) { }
 
   ngOnInit() {
   	this.getPost();
-  	console.log(this);
+
   }
 
   getPost(){
-  	const id = this.route.snapshot.paramMap.get('id')
+  	const id = this.route.snapshot.paramMap.get('id');
   	return this.postService
       .getPostData(id)
-      .subscribe(data=>this.post = data)
+      .subscribe(data=> {
+        data.published = new Date((data.published as any).seconds*1000);
+        this.post = data
+      });
   }
 
   updatePost(){
     const formData = {
       title:this.post.title,
       content:this.post.content
-    }
-    const id = this.route.snapshot.paramMap.get('id')
-    this.postService.update(id,formData)
-    this.editing = false
+    };
+    const id = this.route.snapshot.paramMap.get('id');
+    this.postService.update(id,formData);
+    this.editing = false;
 
   }
   delete(){
-    const id = this.route.snapshot.paramMap.get('id')
-    this.postService.delete(id)
-    this.router.navigate(["/blog"])
+    const id = this.route.snapshot.paramMap.get('id');
+    this.postService.delete(id);
+    this.router.navigate(["/blog"]);
   }
 
+  updateContent() {
+    console.log(this.hostElement);
+    this.hostElement.nativeElement.querySelector('div#postContent').innerHTML = this.post.content;
+    return true;
+  }
 
 
 }
